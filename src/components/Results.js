a0ac08f5077a1f72600ed41bc31dc5ed23d32ec0
Daisuke Shibata <shiba12345678@icloud.com>
2023-10-04 10:35:30 +0900

    スタイルの修正

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,11 +9,11 @@ const Results = ({ countryData, loading }) => {
         <Loading />
       ) : (
         <div>
-          <p>日付：{date.slice(0, 10)}</p>
-          <p>新規感染者：{newConfiremed.toLocaleString()}</p>
-          <p>感染者総数：{totalConfiremed.toLocaleString()}</p>
-          <p>新規回復者：{newRecovered.toLocaleString()}</p>
-          <p>回復者総数：{totalRecovered.toLocaleString()}</p>
+          <p>日付：<span>{date.slice(0, 10)}</span></p>
+          <p>新規感染者：<span>{newConfiremed.toLocaleString()}</span></p>
+          <p>感染者総数：<span>{totalConfiremed.toLocaleString()}</span></p>
+          <p>新規回復者：<span>{newRecovered.toLocaleString()}</span></p>
+          <p>回復者総数：<span>{totalRecovered.toLocaleString()}</span></p>
         </div>
       )}
     </div>
